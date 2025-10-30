04b09389bbf9ebb4b8645da1d3fc27600e8a10c9
seymurahmadov <seymur.ahmadov@azeronline.net>
2025-10-30 12:19:03 +0400

    Initial commit

---
+++ src/main/java/com/example/myapp/controller/MyAppController.java
@@ -0,0 +1,16 @@
+package com.example.myapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/my-app")
+public class MyAppController {
+
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
